f669bcf6147096a694dae72be17bf0e4ea2db60a
ABDULLAH ADAILEH <169062688+abdullahadaileh@users.noreply.github.com>
2024-06-29 11:28:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+
+
+// البدء بجزء تحميل الصفحة وتعريف ال Sections 
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const signupForm = document.getElementById('signupForm');
     const signinForm = document.getElementById('signinForm');
+// ***************************************************************************************
 
+
+
+// بدء sign up form
     if (signupForm) {
         signupForm.addEventListener('submit', function(event) {
             event.preventDefault();
+            // تعريف محتويات ادخال المعلومات الشخصية
             const firstName = document.getElementById('firstName').value;
             const lastName = document.getElementById('lastName').value;
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
+            // *****************************************************************************
+
+
+            // Alert يظهر عند ادخال المعلومات وال
 
             if (firstName && lastName && email && password) {
                 alert('Sign Up successful!');
@@ -18,6 +31,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 alert('Please fill out all fields.');
             }
         });
+            // *******************************************************************************
+
+
+        // عند النقر على الارسال يتم اخفاء نموذج ال sign up  واظهار نموذج ال sign in 
 
         const signinLink = document.getElementById('signinLink');
         signinLink.addEventListener('click', function(event) {
@@ -26,6 +43,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
             document.getElementById('signin-container').classList.remove('hidden');
         });
     }
+        // *************************************************************************************
+
+
+
+        
+        // بدء sign in form 
 
     if (signinForm) {
         signinForm.addEventListener('submit', function(event) {
@@ -33,9 +56,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const email = document.getElementById('signinEmail').value;
             const password = document.getElementById('signinPassword').value;
 
+
+            // Alert عند اكتمال تسجيل الدخول 
             if (email && password) {
                 alert('Sign In successful!');
-                // قم بإعادة توجيه المستخدم إلى صفحة أخرى بعد تسجيل الدخول بنجاح
+
+
             } else {
                 alert('Please fill out all fields.');
             }
@@ -49,3 +75,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 });
+
